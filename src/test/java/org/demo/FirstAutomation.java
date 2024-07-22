7793abc5f606c6f7d137a1f9accfedd3c07e3bb2
Hepzi321 <Hepzibahyuvaraj.4@gmail.com>
2024-07-22 11:50:16 +0530

    My new project for all Files

---
+++ src/test/java/org/demo/FirstAutomation.java
@@ -0,0 +1,21 @@
+package org.demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FirstAutomation {
+	
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.facebook.com");
+		
+	}
+	
+
+}
